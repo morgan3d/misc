4edbfd4892fb4332c9c17538d943c78faeb68d8c
Morgan McGuire <mcguire@nvidia.com>
2018-07-22 00:27:38 -0400

    reduced to 2k characters

--- jsmenu/menu.js
+++ jsmenu/menu.js
@@ -1,16 +1,11 @@
 // Polyfill
 if (HTMLCollection.prototype.forEach === undefined) { HTMLCollection.prototype.forEach = [].forEach }
 
-function showMenu(e) {
-    closeAllMenus()
-    this.classList.add('activeMenu')
-    e.stopPropagation()
-}
+function showMenu(e) { closeAllMenus(); this.classList.add('activeMenu'); e.stopPropagation() }
 
 function closeAllMenus() { document.getElementsByClassName('activeMenu').forEach(function (node) { node.classList.remove('activeMenu') }) }
 
-(function() {
-function on(e, f) { window.addEventListener(e, f) }
+(function() {function on(e,f){window.addEventListener(e,f)}
 
 // Show menu event handler
 on('load', function (event) { document.getElementsByClassName('menuLabel').forEach(function(node) { node.onclick = showMenu }) })
