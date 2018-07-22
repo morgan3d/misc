125d943db87d698ab5e67889684782d1bfdc2755
Morgan McGuire <mcguire@nvidia.com>
2018-07-22 00:10:59 -0400

    .

--- jsmenu/menu.js
+++ jsmenu/menu.js
@@ -1,104 +1,52 @@
 // Polyfill
-if (typeof HTMLCollection.prototype.forEach === "undefined") {
-    HTMLCollection.prototype.forEach = Array.prototype.forEach;
-}
+if (HTMLCollection.prototype.forEach === undefined) { HTMLCollection.prototype.forEach = [].forEach }
 
 function showMenu(event) { event.target.classList.add('activeMenu'); }
 
-function closeAllMenus() {        
-    document.getElementsByClassName('activeMenu').forEach(function (node) { node.classList.remove('activeMenu'); });
-}
+function closeAllMenus() { document.getElementsByClassName('activeMenu').forEach(function (node) { node.classList.remove('activeMenu') }) }
 
-// Add the show menu event handler
-window.addEventListener('load', function (event) { document.getElementsByClassName('menuLabel').forEach(function(node) { node.onclick = showMenu; }); });
+(function() {
+function on(n,f){window.addEventListener(n,f)}
+
+// Show menu event handler
+on('load', function (event) { document.getElementsByClassName('menuLabel').forEach(function(node) { node.onclick = showMenu }) })
 
 // Close all open menus on click or escape
-window.addEventListener('click', closeAllMenus, true);
-window.addEventListener('keydown', function (event) { (event.key === "Escape") && closeAllMenus(); }, true);
+on('click', closeAllMenus, true)
+on('keydown', function (event) { (event.key === "Escape") && closeAllMenus() }, true)
 
-(function () {
-    var style = document.createElement('style');
-    style.type = 'text/css';
-    style.innerHTML = `
-#menubar {
-position: absolute;
-top: 43px;
-font-size: 13px;
-user-select: none;
-}
+var style = document.createElement('style')
+style.type = 'text/css'
+style.innerHTML =`
+#m{position:absolute;top:43px;font-size:13px;user-select:none}
 
-#menubar div {
-display: inline-block;
-vertical-align: top;
-}
+#m div{display:inline-block;vertical-align:top}
 
 /* Menu label (no hover) */
-#menubar .menuLabel { 
-padding: 4px 8px 4px 8px;
-margin-right: 10px;
-margin-left: -8px;
-}
+#m .menuLabel{padding:4px 8px 4px 8px;margin-right:10px;margin-left:-8px}
 
 /* Menu label (hover) */
-#menubar .menuLabel:hover {
-background: #eeeeee;
-}
-
+#m .menuLabel:hover{background:#eee}
 
 /* Drop-down menu (hidden) */
-#menubar div div {
-position: absolute;
-display: block;
-min-width: 120px;
-overflow: auto;
-background: #fff;
-box-shadow: 0px 2px 6px 0px rgba(0,0,0,0.5);
-z-index: 4;
-padding: 4px 2px 2px 30px;
-margin-left: -8px;
-margin-top: 4px;
-}
+#m div div{position:absolute;display:block;min-width:120px;overflow:auto;background:#fff;box-shadow:0px 2px 6px 0px rgba(0,0,0,.5);z-index:4;padding:4px 2px 2px 30px;margin-left:-8px;margin-top:4px}
 
 /* Drop-down menu (hidden) */
-#menubar span:not(.activeMenu) + div {
-visibility:hidden;
-}
+#m span:not(.activeMenu) + div{visibility:hidden}
 
 /* Menu item */
-#menubar div div a {
-color: #000;
-text-decoration: none;
-display: block;
-padding: 4px;
-margin-top: 4px;
-margin-bottom: 4px;
-}
+#m div div a{color:#000;text-decoration:none;display:block;padding:4px;margin-top:4px 0 4px}
 
 /* Left-aligned icon within a menu item */
-#menubar div div a .icon {
-position: absolute;
-left: 9px;
-}
+#m div div a .icon{position:absolute;left:9px}
 
 /* Hotkey documentation in a menu item */
-#menubar div div a .hotkey {
-position: absolute;
-right: 9px;
-color: rgba(0,0,0,0.5);
-}
+#m div div a .hotkey{position:absolute;right:9px;color:rgba(0,0,0,.5)}
 
 /* Google's icon font within the menu */
-#menubar div div a .material-icons {
-font-size: 120%;
-color: rgba(0,0,0,0.55);
-}
+#m div div a .material-icons{font-size:120%;color:rgba(0,0,0,.55)}
 
 /* Line between menu items */
-#menubar div div hr {
-border: none;
-height: 1px;
-background: rgba(0,0,0,0.15);
-margin-top: 5px -2px 5px -30px;
-}`;
-    document.getElementsByTagName('head')[0].appendChild(style);
+#m div div hr{border:none;height:1px;background:rgba(0,0,0,.15);margin-top:5px -2px 5px -30px}`.replace(/#m/g, '#menubar')
+document.getElementsByTagName('head')[0].appendChild(style)
 })();
--- jsmenu/readme.md
+++ jsmenu/readme.md
@@ -1,5 +1,5 @@
 
-An extremely lightweight JavaScript library for dropdown menus by Morgan McGuire. Just insert the `menu.js` file into your program and then build the menu out of `div` and `a` elements as shown in `demo.html`. Only 3kb of BSD-licensed open source code. The default styling is based 
+An extremely lightweight JavaScript library for dropdown menus by Morgan McGuire. Just insert the `menu.js` file into your program and then build the menu out of `div` and `a` elements as shown in `demo.html`. Only 2 kB of BSD-licensed open source code. The default styling is based 
 on Google Docs.
 
 ![](demo.png)
