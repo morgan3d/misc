b6764b71b7d0e6279723cf0f6f5887ac49fb6beb
morgan3d <morgan3d@gmail.com>
2018-11-01 21:05:41 -0400

    tests

--- jsvectorify/test/test.js
+++ jsvectorify/test/test.js
@@ -16,3 +16,15 @@ console.log(src);
 console.log('------------------------------------------------------');
 console.log(dst);
 
+function test(src, desired) {
+    let actual = eval(vectorify(src));
+    console.assert(actual === desired, '"' + src + '" produced ' + actual + ', should have been ' + desired);
+    console.log('test:  (' + src + ') === ' + desired);
+}
+
+test(`dot({x:1,y:2}, {x:4,y:3})`, 10);
+test(`({x:1,y:5} + {x:4,y:3}).y`, 8);
+test(`({x:1,y:2} + {x:4,y:3}).y`, 5);
+test(`([1,2] + [4,3])[0]`, 5);
+test(`([3] * [6])[0]`, 18);
+console.log('Done with tests');
