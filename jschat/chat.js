acda33cc2a1f31dd598e4624f844e1e663b59b56
Morgan <morgan3d@gmail.com>
2020-10-19 09:50:31 -0400

    Two way video

--- jschat/chat.js
+++ jschat/chat.js
@@ -1,14 +1,29 @@
 'use strict';
 
-function getMedia() {
+function startWebCam(callback) {
     navigator.mediaDevices.getUserMedia({audio: true, video: {facingMode: "user"}})
-        .then(function(mediaStream) {
-            console.log('ok!');
-        })
+        .then(callback)
         .catch(function(err) {
             console.log(err);
         });
 }
+
+
+function addWebCamView(caption, mediaStream, playAudio) {
+    const videobox = document.getElementById('videobox');
+
+    const frame = document.createElement('div');
+    frame.innerHTML = caption;
+    const video = document.createElement('video');
+    video.setAttribute('autoplay', true);
+    video.setAttribute('controls', true);
+    video.srcObject = mediaStream;
+    video.style.width = '50%';
+    video.muted = ! playAudio;
+    frame.appendChild(video);
+    videobox.appendChild(frame);
+}
+
 
 /* Write to the clipboard. Hard-coded to the specific URL box */
 function clipboardCopy(text) {
@@ -19,28 +34,48 @@ function clipboardCopy(text) {
     setTimeout(function () { urlTextBox.blur(); });
 }
     
+function startGuest() {
+    let hostID = window.location.search.substring(1);
+    document.getElementById('urlbox').innerHTML = `You are a guest of ${hostID}.`;
+    
+    startWebCam(function (mediaStream) {
+        addWebCamView('You', mediaStream, false);
+        let call = peer.call(hostID, mediaStream);
+        
+        call.on('stream', function (hostStream) {
+            addWebCamView('Host', hostStream);
+        });
+    });
+}
+
+
+function startHost() {
+    peer.on('open', function(id) {
+        const url = 'https://morgan3d.github.io/misc/jschat/?' + id;
+        document.getElementById('urlbox').innerHTML =
+            `You are the host. Others can join at:<br><span style="white-space:nowrap; cursor: pointer; font-weight: bold" onclick="clipboardCopy('${url}')" title="Copy to Clipboard"><input title="Copy to Clipboard" type="text" value="${url}" id="urlTextBox">&nbsp;<b style="font-size: 125%">⧉</b></span>`;
+    });
+    
+    startWebCam(function (mediaStream) {
+        addWebCamView('You', mediaStream, false);
+        
+        peer.on('call', function(call) {
+            // Answer the call, providing our mediaStream
+            call.answer(mediaStream);
+            call.on('stream', function (guestStream) {
+                addWebCamView('Guest', guestStream);
+            });
+        });
+    });
+}
+
 
 let peer = new Peer();
 
 function main() {
     if (window.location.search !== '') {
-        // Guest
-        let hostID = window.location.search.substring(1);
-        document.getElementById('urlbox').innerHTML = `You are a guest of ${hostID}.`;
-        
-        // let conn = peer.connect(hostID);
-        
+        startGuest();        
     } else {
-        // Host
-        peer.on('open', function(id) {
-            const url = 'https://morgan3d.github.io/misc/jschat/?' + id;
-            document.getElementById('urlbox').innerHTML =
-                `You are the host. Others can join at:<br><span style="white-space:nowrap; cursor: pointer; font-weight: bold" onclick="clipboardCopy('${url}')" title="Copy to Clipboard"><input title="Copy to Clipboard" type="text" value="${url}" id="urlTextBox">&nbsp;<b style="font-size: 125%">⧉</b></span>`;
-        });
-        
-        peer.on('call', function(call) {
-            // Answer the call, providing our mediaStream
-            call.answer(mediaStream);
-        });
+        startHost();
     }
 }
